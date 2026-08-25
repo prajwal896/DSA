d493696a3b2908f0ef22d5d796b8b4d9d2d5e40d
Prajwal adaki <135981572+prajwal896@users.noreply.github.com>
2026-08-26 00:05:50 +0530

    Time: 0 ms (100%), Space: 44.5 MB (99.08%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,22 +1,21 @@
-class Solution { 
-    public int missingMultiple(int[] nums, int k) { 
-        int i=0,j=1; 
-        boolean found=false; 
-        
-        for(j=1; found=true; j++){ 
-            found=false;
-            for(int n : nums){ 
-                if(k*j == n) {
+class Solution {  
+    public int missingMultiple(int[] nums, int k) {  
+        int i=0, j=1;  
+        boolean found=false;  
+
+        for(j = 1; ; j++) { 
+            found = false; 
+ 
+            for(int n : nums) { 
+                if(k * j == n) { 
                     found = true; 
                     break; 
-                }
+                } 
             } 
-            
-            if(found == false){ 
-                return k*j; 
+ 
+            if(found == false) { 
+                return k * j; 
             } 
         } 
-        
-        return k; 
-    } 
+    }  
 }
