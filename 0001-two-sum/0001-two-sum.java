8771d1108098a8e8c205cad67e38c07733227c9f
Prajwal adaki <135981572+prajwal896@users.noreply.github.com>
2026-08-25 20:33:37 +0530

    Time: 45 ms (27.29%), Space: 47.1 MB (59.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,10 +3,8 @@ class Solution {
         int i=0, j=0, l=nums.length, p=0, sum=0;  
  
         for(i=0; i<=l-1; i++){  
-            for(j=i+1; j<=l-1; j++){     
-                sum = nums[i] + nums[j]; 
-                 
-                if(sum == target){ 
+            for(j=i+1; j<=l-1; j++){    
+                if(nums[i] + nums[j] == target){ 
                     return new int[]{i, j}; 
                 } 
             }  
